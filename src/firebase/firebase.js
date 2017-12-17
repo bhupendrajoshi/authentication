7171e88b1f33a4f1ef7e40e387d9c66fd8e997eb
bhupendrajoshi <bhupendra.joshi@gmail.com>
2017-12-17 21:51:28 +0100

    react authentication using firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+
+let config = {
+  apiKey: "AIzaSyCW5Ie2k_1HZgitOQICSNumKFday65Qq7Y",
+  authDomain: "react-authentication-dc6b3.firebaseapp.com",
+  databaseURL: "https://react-authentication-dc6b3.firebaseio.com",
+  projectId: "react-authentication-dc6b3",
+  storageBucket: "react-authentication-dc6b3.appspot.com",
+  messagingSenderId: "684888042168"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+  auth,
+};
